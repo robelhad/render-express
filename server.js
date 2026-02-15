92296d1401ccd8d457378ed129f38298f6c866bd
dave <robel_h@yahoo.com>
2026-02-15 21:40:39 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req,res) =>{
+
+res.send("Subscribe to this channel");
+
+
+
+}
+
+
+
+);
+
+app.listen(port, () => {
+    console.log("Server started")
+});
